455c4bf3654106fb29aec226a729abb116751ee0
santiagomez12 <96155613+santiagomez12@users.noreply.github.com>
2022-03-21 13:24:48 -0500

    Add files via upload

---
+++ import matplotlib.pyplot as plt.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+plt.plot([4,8,13,17,20],[54, 67, 98, 78, 45])
+plt.show()
+
+
+import matplotlib.pyplot as plt
+x = [2,4,6,7,9,13,19,26,29,31,36,40,48,51,57,67,69,71,78,88]
+y = [54,72,43,2,8,98,109,5,35,28,48,83,94,84,73,11,464,75,200,54]
+plt.scatter(x,y)
+plt.show()
